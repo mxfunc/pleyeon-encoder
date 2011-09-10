b6bb42d9c7fba8d7a076bc6324ed8e2154eb6968
Deepak Angrula <autodidact.in@gmail.com>
2011-09-10 16:51:35 +0530

    adding file names to zencoder jobs. filename = random string + now_uts + label

--- encoder/utils.py
+++ encoder/utils.py
@@ -21,9 +21,12 @@ MEDIA_FILES_SUFFIXES = [
 #### ZENCODER RESPONSE CODES ######
 ZENCODER_JOB_OK = 201
 
+def now_uts():
+    return time.mktime(datetime.datetime.now())
+
 #### A HACK #########
 def is_being_written(f):
-    return abs(time.mktime(datetime.datetime.now().timetuple())-os.stat(f).st_mtime) < 10
+    return abs(time.mktime(now_uts())-os.stat(f).st_mtime) < 10
 
 def listdir(d=HOME_DIR, size=True):
     list_of_files = []
@@ -36,6 +39,23 @@ def listdir(d=HOME_DIR, size=True):
             list_of_files.append(t)
     return list_of_files
 
+def get_unique_output_name():
+    import random
+    while True:
+        key = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(32))
+        try:
+            o = ZencoderOutput.objects.get(file=key)
+        except ZencoderOutput.DoesNotExist:
+            return key
+        else:
+            continue
+
+def sha256_digest(s):
+    import hashlib
+    sha256 = hashlib.sha256()
+    sha256.update(s)
+    return sha256.hexdigest()
+
 class JobNotCreatedException(Exception):
     pass
 
@@ -44,9 +64,12 @@ def zencoder_add_job(file_path, input_url, upload_prefix, notify_url=None, video
     if notify_url:
         notifications = {"notifications":notify_url}
     base_url = {'base_url':upload_prefix}
+    file_prefix = sha256_digest(file_path)[:32]+'_'+now_uts() 
 
     outputs = []
     for profile in video_profiles:
+        filename = {'filename':file_prefix+'_'+profile['label']} 
+        profile.update(filename)
         profile.update(notifications)
         profile.update(base_url)
         outputs.append(profile)
@@ -66,7 +89,8 @@ def zencoder_add_job(file_path, input_url, upload_prefix, notify_url=None, video
 def zencoder_update_table(job, source_file_path):
     o = ZencoderJob.objects.create(zencoder_id=job.body['id'], file=source_file_path)
     for output in job.body['outputs']:
-        o.outputs.add(ZencoderJobOutput.objects.create(file=source_file_path, zencoder_id=output['id'], url=output['url'], label=output['label']))
+        filename = output['url'].split('/',1)[1]
+        o.outputs.add(ZencoderJobOutput.objects.create(file=filename, zencoder_id=output['id'], url=output['url'], label=output['label']))
     o.save()
 
 
