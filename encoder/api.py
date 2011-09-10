0a4e2d359522f7d7acb0250a5b04451b1d58bd93
Deepak Angrula <autodidact.in@gmail.com>
2011-09-10 17:16:38 +0530

    make files in zencoder work readable by world so that the ftp user can read them

--- encoder/api.py
+++ encoder/api.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-import os, shutil
+import os, shutil, stat
 
 from encoder.utils import zencoder_add_job
 
@@ -11,14 +11,15 @@ def zencoder_submit_sync(file_path, thumbnails=True):
         notify_url = settings.ZENCODER_NOTIFY_URL
         upload_prefix = settings.ZENCODER_UPLOAD_PREFIX
         
+        file_name = os.path.basename(file_path)
         if os.path.isdir(work_dir):
             shutil.copy(file_path, work_dir)
+            os.chmod(os.path.join(work_dir, file_name), stat.S_IROTH)
         else:
             raise ValueError("ENCODER_WORK_DIR is not a directory")
 
         input_url_prefix = settings.ZENCODER_DOWNLOAD_PREFIX
-        input_path = os.path.basename(file_path)
-        input_url = input_url_prefix + input_path if input_url_prefix.endswith('/') else input_url_prefix + '/' + input_path 
+        input_url = input_url_prefix + input_path if input_url_prefix.endswith('/') else input_url_prefix + '/' + file_name 
         zencoder_add_job(file_path, input_url, upload_prefix, notify_url, video_profiles, thumbnail_profiles)
     else:
         raise ValueError("file_path does not exist or is not a file")
